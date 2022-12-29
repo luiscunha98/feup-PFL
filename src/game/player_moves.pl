cee936e7d78faa4c8a327d7e4da73f74d7dd73f1
wolfCuanhamaRWS <up201509918@fc.up.pt>
2022-12-29 03:54:29 +0000

    update more rules to create predicates

--- src/game/player_moves.pl
+++ src/game/player_moves.pl
@@ -4,6 +4,8 @@
 % possible moves that can be made by the player:
 %moves need to be like the below definition and be connected directly or indirectly (through other nodes) to the node that the player is currently on
 
+%check if the
+
 %vertical move
 % if the node chosen is empty, none of the pieces are in the way, and the move is vertical, then the move is valid
 
@@ -14,7 +16,18 @@
 %if the node chosen is empty, none of the pieces are in the way, and the move is circular, then the move is valid
 %moves between the extreme nodes of rows 1,2,3, 7, 8, 9 and columns 1, 2, 3, 7, 8, 9 of rows 4, 5, and 6 are  circular moves
 
+%check if the move is valid:
+%if any of the 3 above is true:
+    %for the one is true:
+        %check if any of the opponent's pieces are in the way of the move (on the circular, horizontal, or vertical)
 
-%horizontal move
+% after checking move validation we change the board configuration ( new board/different nodes graph values) and update the board(change the node value to the player's value representing the player's piece on the board)
+% otherwise if none of the 3 is true, then the move is invalid and the player cannot move to that node we ask for a new possible valid move
 
-%circular move
+%check if one of the current turn player's pieces is totally surrounded by the opponent's pieces
+
+
+
+
+
+%
